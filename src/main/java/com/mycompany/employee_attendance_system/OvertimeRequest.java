35f4de0e87834fe37a521f34cf860e949eb73600
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2024-05-11 10:47:32 +0800

    WIP

---
+++ src/main/java/com/mycompany/employee_attendance_system/OvertimeRequest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.employee_attendance_system;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Neil Patrick
+ */
+public class OvertimeRequest {
+
+    int request_id;
+    Date startDate;
+    Date endDate;
+    String status;
+    String notes;
+    int leave_type_id;
+    int employee_id;
+    Employee employee;
+    LeaveType leaveType;
+    
+    public static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+
+
+    public LeaveRequest(int request_id, Date startDate, Date endDate, String notes, String status, int leave_type_id, int employee_id) {
+        this.request_id = request_id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.status = status;
+        this.notes = notes;
+        this.employee_id = employee_id;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+    
+}
